297d98f0067a31a126a45682a971c5b40b13a6ae
phelippemichel <phelippe.michel@gmail.com>
2024-01-06 20:54:24 -0300

    feat/add list extensions

--- UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -2,7 +2,12 @@ namespace UtmBuilder.Core.Extensions;
 
 public static class ListExtensions
 {
-    public static void AddIfNotNull(this List<string> list)
+    public static void AddIfNotNull(
+        this List<string> list,
+        string key,
+        string? value)
     {
+        if(!string.IsNullOrEmpty(value))
+            list.Add($"{key}={value}");
     }
 }
