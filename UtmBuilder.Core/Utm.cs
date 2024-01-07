d9d4f1af4b48a5d0e769b0f01b1243c67d57921c
phelippemichel <phelippe.michel@gmail.com>
2024-01-07 11:38:13 -0300

    feat/add implicit operators

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,5 +1,6 @@
 using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core;
 
@@ -12,18 +13,44 @@ public class Utm
         Url = url;
         Campaign = campaign;
     }
-    
+
     /// <summary>
     /// URL Website link
     /// </summary>
     public Url Url { get; }
-    
+
     /// <summary>
     /// Campaign Details
     /// </summary>
     public Campaign Campaign { get; }
-    
-    public override string ToString()
+
+    public static implicit operator string(Utm utm)
+        => utm.ToString();
+
+    public static implicit operator Utm(string link)
+    {
+        if (string.IsNullOrEmpty(link))
+            throw new InvalidCampaignException();
+        
+        var url = new Url(link);
+        var segments = url.Address.Split('?');
+        if(segments.Length == 1)
+            throw new InvalidCampaignException("No segments were provided");
+        
+        var pars = segments[1].Split('&');
+        var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
+        var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+        var name = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault("").Split("=")[1];
+        var id = pars.Where(x => x.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];
+        var term = pars.Where(x => x.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];
+        var content = pars.Where(x => x.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+        var utm = new Utm(
+            new Url(segments[0]), 
+            new Campaign(source, medium, name, id, term, content));
+        return utm;
+    }
+public override string ToString()
     {
         var segments = new List<string>();
             segments.AddIfNotNull("utm_source", Campaign.Source);
