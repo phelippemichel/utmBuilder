c3b34180ae63e318b566965bbb359923b8d163c2
phelippemichel <phelippe.michel@gmail.com>
2024-01-10 15:17:06 -0300

    refactor/delete white lines and name of variables

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -13,31 +13,31 @@ public class Utm
         Url = url;
         Campaign = campaign;
     }
-
+    
     /// <summary>
-    /// URL Website link
+    /// URL (Website Link)
     /// </summary>
     public Url Url { get; }
-
+    
     /// <summary>
     /// Campaign Details
     /// </summary>
     public Campaign Campaign { get; }
 
-    public static implicit operator string(Utm utm)
+    public static implicit operator string(Utm utm) 
         => utm.ToString();
 
     public static implicit operator Utm(string link)
     {
         if (string.IsNullOrEmpty(link))
-            throw new InvalidCampaignException();
-        
+            throw new InvalidUrlException();
+
         var url = new Url(link);
-        var segments = url.Address.Split('?');
-        if(segments.Length == 1)
-            throw new InvalidCampaignException("No segments were provided");
-        
-        var pars = segments[1].Split('&');
+        var segments = url.Address.Split("?");
+        if (segments.Length == 1)
+            throw new InvalidUrlException("No segments were provided");
+
+        var pars = segments[1].Split("&");
         var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
         var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
         var name = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault("").Split("=")[1];
@@ -50,15 +50,16 @@ public class Utm
             new Campaign(source, medium, name, id, term, content));
         return utm;
     }
-public override string ToString()
+    
+    public override string ToString()
     {
         var segments = new List<string>();
-            segments.AddIfNotNull("utm_source", Campaign.Source);
-            segments.AddIfNotNull("utm_medium", Campaign.Medium);
-            segments.AddIfNotNull("utm_campaign", Campaign.Name);
-            segments.AddIfNotNull("utm_id", Campaign.Id);
-            segments.AddIfNotNull("utm_term", Campaign.Term);
-            segments.AddIfNotNull("utm_content", Campaign.Content);
-        return $"{Url.Address}?utm_source={string.Join("&", segments)}";
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term);
+        segments.AddIfNotNull("utm_content", Campaign.Content);
+        return $"{Url.Address}?{string.Join("&", segments)}";
     }
 }
